fa7268e7e25c21af3e7d39ec98d30f124a1013a2
AgnishPOC <agnishshyam@outlook.com>
2016-10-31 11:27:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+//Storing data for Object person
+
+var person = {
+    name:"Agnish",
+    age:27,
+    dob:"19-06-1989", // Data of Birth
+    pob:"Kolkata" //Place of Birth
+};
+
+// Logging Details of Person 
+
+console.log("Name : "+person.name+"\n");
+console.log("Age : "+person.age+"\n");
+console.log("Date of Birth : "+person.dob+"\n");
+console.log("Place of Birth : "+person.pob);
+
+alert("Please check console for values");
+
